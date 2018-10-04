1e20ed7a7b2e374fd2ea4984cdb2eb140f64e65a
wilsontom <wilsontom89@gmail.com>
2018-10-04 11:04:40 +0100

    add additional test for checking website status

--- tests/testthat/test-atc.R
+++ tests/testthat/test-atc.R
@@ -2,6 +2,7 @@ context('ATC-codes')
 
 test_that('ATC-codes', {
 
+  expect_true(httr::GET('https://www.whocc.no/atc_ddd')$status_code == 200)
   expect_true(is.data.frame(search_atc_code('A10BA02')))
   expect_error(search_atc_code('A02B'))
 
